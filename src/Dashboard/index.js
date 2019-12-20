94fa6047795a1958782177d9b3e23d7dad33bd44
John Akins <JohnAkins.dev@gmail.com>
2019-12-20 18:22:09 +0100

    feature(profile):add admin profile component UI [finishes #11]

--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -9,16 +9,46 @@ import Feeds from '../Feeds'
 import Article from '../Articles'
 import Profile from '../Profile'
 import UserWrapper from '../Users'
+import fetch from '../MakeRequest'
+
+const makeRequest = fetch.makeRequest;
 
 class Dashboard extends Component {
   constructor(props) {
     super(props)
+    this.M = window.M
     this.logout = this.logout.bind(this)
+    this.makeRequest = makeRequest
     this.state = {
-      activeFeedId : null
+      activeFeedId : null,
+      user : null
     }
   }
   
+
+  componentDidMount() {
+    makeRequest("GET", `${this.props.api}users`,{}, this.props.userSecrets)
+    .then((response) => {
+        if(response.status === "success") {
+          this.setUserData(response.data, this.props.userSecrets.userId)
+        } else {
+          this.M.toast({html: response.error, classes: 'rounded danger white-text'})                 
+        }
+    })
+    .catch((error) => {
+      this.M.toast({html: "request failed, please try again"+error, classes: 'rounded danger'})
+    })
+    return false;    
+  }
+
+  setUserData(users, userId) {
+    users.forEach((user) => {
+      if(user.userId === userId) {
+        return this.setState({ user })
+      }
+    })
+  }
+
   logout() {
     this.props.logout()
   }
@@ -78,7 +108,7 @@ class Dashboard extends Component {
             <Feeds api={ this.props.api } userSecrets={ this.props.userSecrets } />
           </Route>
           <Route exact path="/profile">
-            <Profile api={ this.props.api } userSecrets={ this.props.userSecrets } />
+            <Profile user = {this.state.user} />
           </Route>
           <Route exact path="/users">
             <UserWrapper api={ this.props.api } userSecrets={ this.props.userSecrets } />
--- src/Profile/index.js
+++ src/Profile/index.js
@@ -1,17 +1,45 @@
 import React, {Component} from 'react';
 import '../App.css';
 
-
+const RESET_VALUES = { firstName: "",lastName: "",email: "",address: "",gender: "",jobRole: "",department: "",isAdmin: true,userId: "",imageUrl: null }
 
 class Profile extends Component {
     constructor(props) {
         super(props)
         this.M = window.M;
+        this.state = {
+            user: RESET_VALUES
+        }
     }
 
     componentDidMount() {
-        this.M.AutoInit()        
+        this.M.AutoInit()
+        if(this.props.user !== null) {
+            this.setState((prevState) => {
+                prevState.user = this.props.user
+                return { prevState }
+            })
+        }
     }
+
+    componentDidUpdate(prevProps) {
+        if(prevProps.user !== this.props.user) {
+            this.setState((prevState) => {
+                prevState.user = this.props.user
+                return { prevState }
+            })      
+
+            const inputs = document.querySelectorAll('.profile-data-card input[type=text].validate')
+            setTimeout(()=>{
+                inputs.forEach((input) => {
+                    input.nextSibling.classList.add('active')
+                })
+                var elems = document.querySelectorAll('.profile-data-card select');
+                this.M.FormSelect.init(elems);              
+            },500)            
+        }
+    }
+
 
     render() {
         return (
@@ -35,21 +63,23 @@ class Profile extends Component {
                                 <h6>Bio:</h6>
                             <div className="section" >
                                 <div className="input-field col s12 noselect">
-                            <input id="sname" type="text" className="validate valid"/>
-                            <label className="">First Name</label>
+                            <input disabled defaultValue={this.state.user.firstName} id="sname" type="text" className="validate valid"/>
+        <label className="">First Name</label>
                             </div>
                         <div className="divider"></div>
 
                         <div className="input-field col s12 noselect">
-                            <input id="oname" type="text" className="validate valid"/>
+                            <input disabled defaultValue={this.state.user.lastName} id="oname" type="text" className="validate valid"/>
                             <label className="">Last Name</label>
                             </div>
                         <div className="divider"></div>
                         </div>
                         <div className="input-field col s12  selectable">
-                            <select name="gender" id="gender" >
-                                <option value = "1">Male</option>
-                                <option value = "0">Female</option>
+                            <select name="gender" id="gender" disabled >
+                                <option value=""> Gender </option>
+                                <option value = "male" selected={this.state.user.gender === "male" ? true:false} >Male </option>
+                                <option value = "female" selected={this.state.user.gender === "female" ? true:false} >Female
+                                </option>
                             </select>
                             <label>Gender</label>
                         </div>
@@ -60,12 +90,12 @@ class Profile extends Component {
                         <div className="section" >
                                 <div className="col s12 noselect">
                                 <label className="">Address</label>
-                                <textarea id="address" className="materialize-textarea active" placeholder="E.g 4 Maddison street, LA" style={{"boxShadow": "unset"}}></textarea>
+                                <textarea id="address" className="materialize-textarea active" placeholder="E.g 4 Maddison street, LA" style={{"boxShadow": "unset"}} disabled defaultValue={this.state.user.address} ></textarea>
                                 </div>
                         <div className="divider"></div>
 
                         <div className="input-field col s12 noselect">
-                            <input id="email" type="text" className="validate valid"/>
+                            <input disabled  defaultValue={1} id="email" type="text" className="validate valid" defaultValue={this.state.user.email} />
                             <label className="">Email</label>
                             </div>
                         <div className="divider"></div>
@@ -77,32 +107,32 @@ class Profile extends Component {
                             <h6>Role:</h6>
                             <div className="section" >
                                 <div className="input-field col s12  selectable">
-                                <select name="Departments" id="roles" >
+                                <select name="Departments" id="roles" disabled>
                                     <option value="">Select a department</option>
-                                    <option value="1">IT Department</option>
-                                    <option value="2">Human Resources</option>
-                                    <option value="3">Marketing</option>
-                                    <option value="4">Logistics</option>
+                                    <option value="IT" selected={this.state.user.department === "IT" ? true:false} >IT Department</option>
+                                    <option value="Human Resources" selected={this.state.user.department === "Human Resources" ? true:false} >Human Resources</option>
+                                    <option value="Marketing" selected={this.state.user.department === "Marketing" ? true:false}>Marketing</option>
+                                    <option value="Logistics" selected={this.state.user.department === "Logistics" ? true:false}>Logistics</option>
                                 </select>
                                 <label>Department</label>
                                 </div>
 
                                 <div className="input-field col s12  selectable">
-                                <select name="roles" id="roles" >
+                                <select name="roles" id="roles" disabled>
                                     <option value="">Select a designation</option>
-                                    <option value="1">Software Engineer</option>
-                                    <option value="2">Manager</option>
-                                    <option value="3">IT Manager</option>
-                                    <option value="4">CTO</option>
+                                    <option value="Software Engineer" selected={this.state.user.jobRole === "Software Engineer" ? true:false}>Software Engineer</option>
+                                    <option value="Manager" selected={this.state.user.jobRole === "Manager" ? true:false}>Manager</option>
+                                    <option value="IT Manager" selected={this.state.user.jobRole === "IT Manager" ? true:false}>IT Manager</option>
+                                    <option value="CTO" selected={this.state.user.jobRole === "CTO" ? true:false}>CTO</option>
                                 </select>
                                 <label>Job Role</label>
                                 </div>
 
                                 <div className="input-field col s12  selectable">
-                                <select name="priviledge" id="priviledge">
+                                <select name="priviledge" id="priviledge" disabled>
                                     <option value="">Select user priviledge</option>
-                                    <option value="1">Admin</option>
-                                    <option value="2">Employee</option>
+                                    <option value={true} selected={this.state.user.isAdmin === true ? true:false}>Admin</option>
+                                    <option value={false} selected={this.state.user.isAdmin === false ? true:false}>Employee</option>
                                 </select>
                                 <label>Priviledge</label>
                                 </div>
